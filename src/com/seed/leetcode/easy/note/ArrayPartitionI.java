5db33e731c5596d3e413c2083a5a6be82db180e9
jhonprice <a718329809@live.com>
2017-09-14 10:59:28 +0800

    学习了ArrayPartition1的证明过程 学习了NimGame的证明过程

---
+++ src/com/seed/leetcode/easy/note/ArrayPartitionI.java
@@ -0,0 +1,17 @@
+package com.seed.leetcode.easy.note;
+
+import java.util.Arrays;
+
+/**
+ * Created by 若宇 on 2017/9/13.
+ */
+public class ArrayPartitionI {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
--- src/com/seed/leetcode/easy/note/NimGame.java
+++ src/com/seed/leetcode/easy/note/NimGame.java
@@ -1,4 +1,4 @@
-package com.seed.leetcode.easy;
+package com.seed.leetcode.easy.note;
 
 /**
  * Created by 若宇 on 2017/9/13.
